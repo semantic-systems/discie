4d8e22057d529ae556bb0810148734f4930540a9
cedricmoller <cedric.moeller@icloud.com>
2023-09-04 10:31:05 +0200

    Show more information

--- src/demo/main.py
+++ src/demo/main.py
@@ -1,99 +1,73 @@
 import json
+import threading
+
 import dash
 import requests
-from dash import dcc, html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 from pyvis.network import Network
 
+from src.discriminative_cie.discriminative_cie import DiscriminativeCIE
+
 # Create a Dash app
 app = dash.Dash(__name__)
+
+# Create a sample graph using pyvis
+g = Network()
+g.barnes_hut(gravity=-5000, central_gravity=0.3, spring_length=200, spring_strength=0.001, damping=0.09,
+                           overlap=0.5)
+g.add_node("A", label="Node A")
+g.add_node("B", label="Node B")
+g.add_node("C", label="Node C")
+g.add_edge("A", "B", label="Edge AB")
+g.add_edge("B", "C", label="Edge BC")
+g.show("graph.html")
+
+alt_graph = Network()
+alt_graph.add_node("A", label="Node d")
+alt_graph.add_node("B", label="Node as")
+alt_graph.add_node("C", label="Node Cf")
 
 # Define the app layout
 app.layout = html.Div([
     html.H1("DISCIE Demo", style={'textAlign': 'center'}),
 
-    # Textarea for input
-    html.Div([
-        html.Label("Input Text:", style={'fontSize': 16}),
-        dcc.Textarea(
-            id='input-text',
-            placeholder='Enter your text here!',
-            rows=5,
-            style={'width': '100%', 'fontSize': 14}
-        ),
-    ]),
-
-    # Threshold Sliders
-    html.Div([
-        html.Label("Mention Threshold:", style={'fontSize': 16, 'marginRight': '10px'}),
+# Add Divs to display threshold values
+# Textarea for input
+html.Label("Input Text:", style={'fontSize': 16, }),
+html.Div([
+dcc.Textarea(
+    id='input-text',
+    placeholder='Enter your text here!',
+    rows=5,
+    style={'width': '75%', 'fontSize': 14}
+),
+]),
+html.Div([
+html.Label("Mention threshold:", style={'width': '9%', 'margin-right': '1px', 'display': 'inline-block'}),
         html.Div([
-            dcc.Slider(
-                id='threshold1-slider',
-                min=0,
-                max=1,
-                step=0.01,
-                value=0.1,
-                marks={0: '0', 1: '1'},
-                tooltip={'placement': 'top', 'always_visible': True},
-            ),
-        ], style={'width': '80%', 'margin': 'auto'}),
-    ], style={'display': 'flex', 'alignItems': 'center'}),
-
-    html.Div([
-        html.Label("Property Threshold:", style={'fontSize': 16, 'marginRight': '10px'}),
+            dcc.Slider(id='threshold1-slider', min=0, max=1, step=0.01, value=0.1, marks={0: '0', 1: '1'}, tooltip={'placement': 'top', 'always_visible': True}),
+        ], style={'display': 'inline-block', 'width': '71%'}),
+    ], style={'display': 'flex', 'align-items': 'center'}),
+html.Div([
+        html.Label("Property threshold:", style={'width': '9%', 'margin-right': '1px', 'display': 'inline-block'}),
         html.Div([
-            dcc.Slider(
-                id='threshold2-slider',
-                min=0,
-                max=1,
-                step=0.01,
-                value=0.3,
-                marks={0: '0', 1: '1'},
-                tooltip={'placement': 'top', 'always_visible': True},
-            ),
-        ], style={'width': '80%', 'margin': 'auto'}),
-    ], style={'display': 'flex', 'alignItems': 'center'}),
+            dcc.Slider(id='threshold2-slider', min=0, max=1, step=0.01, value=0.3, marks={0: '0', 1: '1'}, tooltip={'placement': 'top', 'always_visible': True}),
+        ], style={'display': 'inline-block', 'width': '71%'}),
+    ], style={'display': 'flex', 'align-items': 'center'}),
 
-    html.Div([
-        html.Label("Combined Threshold:", style={'fontSize': 16, 'marginRight': '10px'}),
+html.Div([
+        html.Label("Combined threshold:", style={'width': '9%', 'margin-right': '1px' ,'display': 'inline-block'} ),
         html.Div([
-            dcc.Slider(
-                id='threshold3-slider',
-                min=0,
-                max=1,
-                step=0.01,
-                value=0.3,
-                marks={0: '0', 1: '1'},
-                tooltip={'placement': 'top', 'always_visible': True},
-            ),
-        ], style={'width': '80%', 'margin': 'auto'}),
-    ], style={'display': 'flex', 'alignItems': 'center'}),
+            dcc.Slider(id='threshold3-slider', min=0, max=1, step=0.01, value=0.3, marks={0: '0', 1: '1'}, tooltip={'placement': 'top', 'always_visible': True}),
+        ], style={'display': 'inline-block', 'width': '71%'}),
+    ], style={'display': 'flex', 'align-items': 'center'}),
 
-    # Update Graph Button
-    html.Div([
-        html.Button(
-            'Update Graph',
-            id='button-update-graph',
-            n_clicks=0,
-            style={'color': 'white', 'backgroundColor': '#009688', 'width': '80%', 'fontSize': 16,
-                   'margin': '20px auto'}
-        ),
-    ]),
 
-    # Graph Output
-    dcc.Loading(
-        id="loading-graph",
-        type="default",
-        children=[
-            html.Iframe(
-                id='graph-output',
-                srcDoc=open("graph.html", "r").read(),
-                width='80%',
-                height='600',
-                style={'border': 'none'}
-            ),
-        ],
-    ),
+    html.Button('Update Graph', id='button-update-graph', n_clicks=0, style={'color': 'white', 'background-color': '#009688', 'width': '80%'}),
+
+    html.Iframe(id='graph-output', srcDoc=open("graph.html", "r").read(), width='80%', height='600'),
 ])
 
 
@@ -107,22 +81,26 @@ def update_graph(n_clicks, input_text, threshold1, threshold2, threshold3):
     ctx = dash.callback_context
     if ctx.triggered[0]['prop_id'] == 'button-update-graph.n_clicks':
         response = requests.post('http://localhost:5001/process_text', json={'text': input_text,
-                                                                             'threshold1': threshold1,
-                                                                             'threshold2': threshold2,
-                                                                             'threshold3': threshold3})
+                                                                              'threshold1': threshold1,
+                                                                              'threshold2': threshold2,
+                                                                              'threshold3': threshold3})
         if response.status_code == 200:
             graph = Network(directed=True)
+            graph.barnes_hut(gravity=-10000, central_gravity=0.3, spring_length=200, spring_strength=0.001, damping=0.09,
+                           overlap=0.5)
+
             graph_data = response.json()
             print(graph_data)
             for node in graph_data['nodes']:
-                graph.add_node(node['id'], label=node['id'])
+                graph.add_node(node['id'], label=f"{node['id']}/{node['type']}")
             for edge in graph_data['edges']:
-                graph.add_edge(edge['source'], edge['target'], label=edge['label'])
+                graph.add_edge(edge['source'], edge['target'], label=f"{edge['property']}/{edge['property_label']}")
 
             graph.show("graph.html")
 
         return open("graph.html", "r").read()
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
+
--- src/demo/provider.py
+++ src/demo/provider.py
@@ -4,6 +4,32 @@ import networkx as nx
 from flask import jsonify, request, Flask
 
 from src.discriminative_cie.discriminative_cie import DiscriminativeCIE
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+wikidata_endpoint = "https://query.wikidata.org/sparql"
+wikidata_sparql = SPARQLWrapper(wikidata_endpoint)
+
+def normalize_identifier(identifier: str):
+    if identifier.startswith("Q"):
+        return identifier
+    elif identifier.startswith("http://www.wikidata.org/entity/"):
+        return identifier.split("/")[-1]
+    else:
+        raise ValueError(f"Invalid identifier: {identifier}")
+def get_labels_via_sparql(identifiers: list):
+    identifiers = " ".join([f"wd:{x}" for x in identifiers])
+    query = f"""
+    SELECT ?item ?label
+    WHERE {{
+    VALUES ?item {{ {identifiers} }}
+      ?item rdfs:label ?label .
+      FILTER(LANGMATCHES(LANG(?label), "en"))
+    }}"""
+    wikidata_sparql.setQuery(query)
+    wikidata_sparql.setReturnFormat(JSON)
+    results = wikidata_sparql.query().convert()
+    return {normalize_identifier(x["item"]["value"]): x["label"]["value"] for x in results["results"]["bindings"]}
 
 
 def instantiate_discie():
@@ -44,13 +70,14 @@ def process_text():
 
     triples, all_occuring_entities = disambiguated[0]
     nodes = list(set(all_occuring_entities))
+    pids = list({triple[1] for triple in triples})
+    all_labels = get_labels_via_sparql(nodes + pids)
     for triple in triples:
-
         edges.append((triple[0], triple[2], triple[1]))
 
     # Convert the graph to a format that can be sent as JSON
-    nodes = [{'id': node} for node in nodes]
-    edges = [{'source': source, 'target': target, "label": label} for source, target, label in edges]
+    nodes = [{'id': node, 'label': all_labels[node]} for node in nodes]
+    edges = [{'source': source, 'target': target, "property": label, "property_label": all_labels[label]} for source, target, label in edges]
 
     response = {
         'nodes': nodes,
@@ -63,6 +90,9 @@ def test():
     print("test")
     return "test"
 
-discie = instantiate_discie()
+
+print(get_labels_via_sparql(["P31", "Q76"]))
+
 if __name__ == '__main__':
+    # discie = instantiate_discie()
     app.run(host='0.0.0.0', port=5001)
